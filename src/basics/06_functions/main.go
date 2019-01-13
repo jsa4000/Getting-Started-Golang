6b488d1241cc31f4d7b6d9f04c3542f2a828562c
jsa4000 <jsa4000@gmail.com>
2019-01-13 10:22:19 +0100

    Finished functions basics in go

--- src/basics/06_functions/main.go
+++ src/basics/06_functions/main.go
@@ -5,7 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"time"
 )
+
+// Conversion from nanoseconds to milliseconds
+const nanoseconds = 1000000
 
 // Greeting const used in the example (private)
 const greeting = "Hello, World"
@@ -96,6 +100,53 @@ func (u *User) SendEmail(text string) error {
 	return nil
 }
 
+// HandlerFunc Definition handler used by the Logger function (decorator)
+// This is a type fon functions (delegates).
+type HandlerFunc func(args string)
+
+// ProcessReq that explicitly uses 'HandlerFunc' type definition
+func ProcessReq(args string) {
+	// Simulate a delay to process the request (aysnc, database, external rpc calls, etc..)
+	time.Sleep(100 * nanoseconds)
+	fmt.Println(args)
+}
+
+// Logger is a decorator (wrapper) around another function (or closure)
+// In golang is oftenly called Middleware, and it takes a pointer to a function
+// (i.e. req := Logger(ProcessReq)) and it returns the same function wrapper into another.
+// Finally the function can be executed normally using the type paremeters req("args")
+//
+// NOTE: Basically, every function with the form 'func(args string)' or 'HandlerFunc' type,
+//       can be used inside this logger or 'decorator'
+func Logger(f HandlerFunc) HandlerFunc {
+	fmt.Println("Creating Logger Handler")
+	defer fmt.Println("Created (returned) Logger Handler")
+
+	// Return the same function passed in parameters but
+	return func(args string) {
+		start := time.Now()
+		fmt.Println("Start Function Handler")
+		// Following line must be enclosed by a lambda function
+		// fmt.Printf("Ends Function Handler in %.2f ms\n", float64(time.Since(start))/nanoseconds) // DOES NOT WORK
+		defer func() { fmt.Printf("Ends Function Handler in %.2f ms\n", float64(time.Since(start))/nanoseconds) }()
+		f(args)
+	}
+}
+
+// Counter is a closure function that holds a variable (state). So in every call the inner status changes
+func Counter() func(int) int {
+	// Initialize state.
+	// Initial state can be passed through parameters if wanted
+	// i.e 'func Counter(start int) int' -> total := start (or simply use 'start' as total counter)
+	total := 0
+	return func(value int) int {
+		// Modify current state with current value
+		total += value
+		// Return current state after the change
+		return total
+	}
+}
+
 func printHeader(text string) {
 	fmt.Println()
 	fmt.Println("*** " + text + " ***")
@@ -170,6 +221,24 @@ func main() {
 
 	printHeader("Lambda Functions")
 
+	// Lambda function or anonymous functions are functions that are declared inside
+	// the function scope, instead declaring it globally
+
+	// A language that considers procedures to be "first-class" allows
+	// functions to be passed around just like any other value.
+
+	// Create a lambda function sum (functions as first-class citizens)
+	sum := func(a, b int) int {
+		return a + b
+	}
+	//Execute previous lambda function
+	total := sum(2, 5)
+	fmt.Printf("Total Sum: %d\n", total)
+
+	// Create and execute a lambda function
+	diff := func(a, b int) int { return a - b }(7, 2)
+	fmt.Printf("Total Diff: %d\n", diff)
+
 	printHeader("Methods")
 
 	// Methods are functions, however they are attached to an struct (like classes)
@@ -200,5 +269,23 @@ func main() {
 	}
 
 	printHeader("Closures")
+
+	// Go functions may be 'closures'. A closure is a function value that references variables
+	// from outside its body. The function may access and assign to the referenced variables;
+	// in this sense the function is "bound" to the variables.
+
+	// Logger is a decorator (wrapper) around another function (this is called 'closure')
+	// In golang is oftenly called Middleware, and it takes a pointer to a function (typed)
+	// (i.e. req := Logger(ProcessReq)), and it returns the same function wrapped into another.
+	request := Logger(ProcessReq) // ProcessReq is the pointer to the function to call later on, inside the closure
+
+	// Finally the function can be executed normally using the 'HandlerFunc' type
+	request("This is a request")
+
+	// Creates a Counter closure to store a stateful counter
+	counter := Counter()
+	fmt.Println(counter(0))
+	fmt.Println(counter(2))
+	fmt.Println(counter(5))
 
 }
